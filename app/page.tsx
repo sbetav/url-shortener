ef07cb16f9f9599b49fc5598e3e403e33da6db93
Santiago <santibeta82@gmail.com>
2025-05-07 16:29:38 -0500

    Add tailwind classname sort plugin

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import { FC } from "react";
 interface PageProps {}
 
 const Page: FC<PageProps> = ({}) => {
-  return <div>Page</div>;
+  return (
+    <main className="flex h-dvh w-full items-center justify-center">Page</main>
+  );
 };
 
 export default Page;
